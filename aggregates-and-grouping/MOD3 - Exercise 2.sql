b83491e3cf5b06ccd83f92807c1fd98cc4a0f74e
HeldanaG <heldanagmariam@gmail.com>
2025-06-04 11:33:34 -0500

    Completed MOD3 - Exercise 2

--- aggregates-and-grouping/MOD3 - Exercise 2.sql
+++ aggregates-and-grouping/MOD3 - Exercise 2.sql
@@ -44,8 +44,8 @@ GROUP BY
 
 -- 8. For suppliers that provide at least 5 items to Northwind, what is the supplier ID of each supplier and the number of items they supply? You can answer this query by only looking at the Products table.
 SELECT 
-    SupplierID, 
-    COUNT(*) AS NumberOfItems
+    SupplierID
+    , COUNT(*) AS NumberOfItems
 FROM 
     northwind.Products
 GROUP BY 
@@ -55,11 +55,11 @@ HAVING
 
 -- 9. List the product id, product name, and inventory value (calculated by multiplying unit price by the number of units on hand). Sort the results in descending order by value. If two or more have the same value, order by product name
 SELECT 
-    ProductID, 
-    ProductName, 
-    UnitPrice * UnitsInStock AS InventoryValue
+    ProductID 
+    , ProductName
+    , UnitPrice * UnitsInStock AS InventoryValue
 FROM 
     northwind.Products
 ORDER BY 
-    InventoryValue DESC,
-    ProductName;
+    InventoryValue DESC
+    , ProductName;
